4fcd41fce6a0d8d40eee12c8499b99e66ac459ba
jamesdcraine <jamesdcraine@gmail.com>
2019-12-30 09:28:17 -0500

    added html index, jss and css

---
+++ questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        title: "The condition in an if/else statement is enclosed within _______.",
+        choices: ["quotes", "square brackets", "parentheses", "curly brackets"],
+        answer: "<script>"
+    },
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<scripting>", "<js>", "<script>", "<javascript>"],
+        answer: "<script>"
+    },
+    {
+        title: "Where is the correct place to insert a JavaScript?",
+        choices: ["The <head> section", "The <body> section", "Both the <head> and <body> sections are correct"],
+        answer: "<script src='abc.js'>"
+    },
+    {
+        title: "How do you write "Hello World" in an alert box?",
+        choices: ["alertBox("Hello World")", "msgBox("Hello World")", "msg("Hello World")", "alert("Hello World")"],
+        answer: "alert("Hello World")"
+    },
+    {
+        title: "What is a DOM in JavaScript?",
+        choices: ["Data of Mine", "Document of Master", "Data Object Modal", "Document Object Model"],
+        answer: "Document Object Model"
+    },
+    {
+        title: "How can you add a comment in a JavaScript?",
+        choices: ["'This is a comment", "//This is a comment", "<!--This is a comment-->", "##This is a comment"],
+        answer: "//This is a comment"
+    },
+];
